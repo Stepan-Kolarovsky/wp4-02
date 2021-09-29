e50b8fbf999f67eb96f68643b944d1c7a16c7a68
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2021-09-29 09:47:46 +0200

    x

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once "classes/Computer.php";
 
 $pc1 = new Computer('ATX Microtower ');
 try {
@@ -7,7 +8,7 @@ try {
     $pc1->addCPU('Intel i5 10800U');
     $pc1->addRAM('Corsair 16 GB dual channel pack');
     $pc1->connectKeyboard('ConnectIT battle RNBW Edition');
-    $pc1->connectMouse('Razer mamba');
+    $pc1->connectMouse('Razer Mamba');
     $pc1->addGPU('Geforce GTX 1030');
     $pc1->connectDisplay('Samsung SQC1050');
     $pc1->addPowerSupply('NZXT 650W');
